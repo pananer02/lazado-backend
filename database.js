58ff7b197327f93b42d14a09ca014c9b656e280d
pananer02 <pananer02@hotmail.com>
2021-04-28 23:21:48 +0700

    ad

--- database.js
+++ database.js
@@ -1,3 +1,3 @@
 module.exports = {
-    db: 'mongodb+srv://pananer:1949356789a@cluster0.4ejcl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+    db: 'mongodb+srv://pananer:1234@cluster0.4ejcl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 }
