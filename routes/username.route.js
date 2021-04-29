d59f99cdf24fa5daf08cbdc8a6e3fedafcc1ad87
pananer02 <pananer02@hotmail.com>
2021-04-29 14:35:39 +0700

    update

--- routes/username.route.js
+++ routes/username.route.js
@@ -1,5 +1,9 @@
 const express = require('express');
+const router = express.Router()
 const usernameRoute = express.Router();
+const bodyPaser = require('body-parser')
+const md5 = require('md5');
+const jwt = require('jsonwebtoken')
 
 // Student model
 let UsernameModel = require('../models/username');
